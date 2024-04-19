30de1860180f0ee4c1ba7a7afd44e5d86cc1f853
steve jones <way0utwest@hotmail.com>
2024-04-19 16:07:23 -0600

    attempt to write parquet

--- DataSet Tools/jsontoparquet/importjson.py
+++ DataSet Tools/jsontoparquet/importjson.py
@@ -1,9 +1,27 @@
 import json
 import pandas as pd 
+import pyarrow as pa
+import pyarrow.parquet as pq
 
-file = 'SQLSat1043.json'
-with open(file) as json_data:
+from os import listdir
+from os.path import isfile, join
+
+mypath = '.'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.json') ]
+print('List of files in current folder')
+print(onlyfiles)
+
+# loop through the files
+#for f in onlyfiles:
+with open(onlyfiles[0]) as json_data:
     data = json.load(json_data)
-    df = pd.DataFrame(data['sessions'])
+        #df = pd.DataFrame(data['sessions'])
 
-print(df.head())
+    # print the head
+    #print(df.head())
+
+#    outputFile = f + '.parquet'
+#    pqtable = pa.Table.from_pandas(df)
+
+    # Write Arrow Table to Parquet file
+#    pq.write_table(pqtable, outputFile)
